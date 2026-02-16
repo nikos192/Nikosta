965280c4f61b5c76a4ed931e84734a9d9f5c750c
nikos192 <nikos_192837@outlook.com>
2026-02-16 22:03:24 +1000

    robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next'
+
+const baseUrl = 'https://velory.systems'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
